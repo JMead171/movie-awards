e8cf291dbb52a369518cad24e70c0ca9a1ef6edf
Jeff Mead <jeffdmead171@gmail.com>
2021-01-08 13:37:32 -0500

    adding logic to remove nominations

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -3,9 +3,12 @@ let posterApi = 'http://img.omdbapi.com/?s=';
 let apiKey = '&apikey=15761393';
 let movieName = "";
 let movieFormEl = document.querySelector('#search-movie');
+let removeNomEl = document.querySelector('.nominations')
 let nominateEl = document.querySelector('.movie-names')
 let errorSearchEl = document.querySelector('.error-msg');
 let movieInputEl = document.querySelector('#movie-input');
+let movieArr = [];
+let movieSearch = {};
 
 
 let getMovies = function(movieName) {
@@ -18,7 +21,6 @@ let getMovies = function(movieName) {
             
             let inputEl = document.getElementById('movie-rm');
             if (inputEl != null) {
-                inputEl.placeholder = " ";
                 inputEl.textContent = " ";
             };
 
@@ -30,23 +32,26 @@ let getMovies = function(movieName) {
             for (let i = 0; i < data["Search"].length; i++) {
                 if (data["Search"][i]["Type"] === "movie") {
                     let displayMovie = data["Search"][i]["Title"];
+                    let displayYear = data["Search"][i]["Year"];
+
                     // Search results
                     let movieNameEl = document.querySelector('.movie-names');
                     let movieNm = document.createElement('p');
                     movieNameEl.appendChild(movieNm);
-                    movieNm.innerHTML = displayMovie;
+                    movieNm.innerHTML = displayMovie + ", " + displayYear;
                     // movieNm.className = "no-outline";
                     // Buttons
                     let movieBtnEl = document.querySelector('.movie-names'); 
                     let movieBtn = document.createElement('button');
                     movieBtnEl.appendChild(movieBtn); 
                     movieBtn.type = 'submit';
-                    movieBtn.value = displayMovie;
+                    movieBtn.value = displayMovie + ", " + displayYear;
                     movieBtn.textContent = "Nominate";
-                    movieBtn.id = "button";
+                    movieBtn.id = "button"+i;
+                    movieSearch[movieNm.innerHTML] = movieBtn.id;
                 }
             }
-
+            console.log("Obj: ", movieSearch);
             });
         } else {
             console(response.statustext);
@@ -71,13 +76,15 @@ let getSearchName = function (event) {
 
 let saveNominee = function (event) {
     event.preventDefault();
+    console.log("Button....: ", event);
+    event.submitter.disabled = "true";
     let displayMovie = event.submitter.value;
     // Search results
     let movieNameEl = document.querySelector('.nominations');
     let movieNm = document.createElement('p');
     movieNameEl.appendChild(movieNm);
     movieNm.innerHTML = displayMovie;
-    // movieNm.className = "no-outline";
+    saveToStorage(displayMovie);
     // Buttons
     let movieBtnEl = document.querySelector('.nominations'); 
     let movieBtn = document.createElement('button');
@@ -88,5 +95,73 @@ let saveNominee = function (event) {
     movieBtn.id = "button"
 };
 
+
+let removeNominee = function (event) {
+    event.preventDefault();
+    console.log("Remove.....", event.submitter.value);
+    let removeMovie = event.submitter.value;
+    if ("Nomination" in localStorage) {
+        let storageData = localStorage.getItem("Nomination");
+        movieArr = JSON.parse(storageData);
+
+        for (let i = 0; i < movieArr.length; i++) {
+            if (removeMovie === movieArr[i]) {
+                movieArr.splice(i,1);
+                localStorage.setItem("Nomination", JSON.stringify(movieArr));
+                let enable = movieSearch[removeMovie];
+                document.getElementById(enable).disabled = false;
+                console.log(movieSearch[removeMovie]);
+                // movieSearch[removeMovie].disabled = 'false';
+
+            }   
+        }
+        let inputEl = document.getElementById('nom-rm');
+        if (inputEl != null) {
+            inputEl.textContent = " ";
+            let nomEl = document.querySelector('.nominations');
+            let nomin = document.createElement('h3');
+            nomEl.appendChild(nomin);
+            nomin.textContent = "Nominations";
+        };
+        loadNominations(); 
+    }
+};
+
+let saveToStorage = function (movie) {
+    if ("Nomination" in localStorage) {
+        let storageData = localStorage.getItem("Nomination");
+        movieArr = JSON.parse(storageData);
+    };
+
+    movieArr.push(movie);
+    localStorage.setItem("Nomination", JSON.stringify(movieArr));
+};
+
+let loadNominations = function () {
+    if ("Nomination" in localStorage) {
+        let storageData = localStorage.getItem("Nomination");
+        movieArr = JSON.parse(storageData);
+
+        for (let i = 0; i < movieArr.length; i++) {
+            let displayMovie = movieArr[i];
+            // Search results
+            let movieNameEl = document.querySelector('.nominations');
+            let movieNm = document.createElement('p');
+            movieNameEl.appendChild(movieNm);
+            movieNm.innerHTML = displayMovie;
+            // Buttons
+            let movieBtnEl = document.querySelector('.nominations'); 
+            let movieBtn = document.createElement('button');
+            movieBtnEl.appendChild(movieBtn); 
+            movieBtn.type = 'submit';
+            movieBtn.value = displayMovie;
+            movieBtn.textContent = "Remove";
+            movieBtn.id = "button"
+        }
+    } 
+};
+
+loadNominations();
 movieFormEl.addEventListener("submit", getSearchName);
 nominateEl.addEventListener("submit", saveNominee);
+removeNomEl.addEventListener("submit", removeNominee);
